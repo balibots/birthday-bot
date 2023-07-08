d48f4a5bae92eec30f1242bcf66030fec52b3a15
Rui Ramos <ruiramos@gmail.com>
2023-07-08 21:09:36 +0100

    Make MSW not warn on requests to dynamodb

---
+++ jest.setup.ts
@@ -0,0 +1,22 @@
+import { server } from "./src/mocks/server";
+
+// Establish API mocking before all tests.
+beforeAll(() =>
+  server.listen({
+    onUnhandledRequest(req, print) {
+      // not warning about requests to dynamodb - we want those to go through
+      if (req.url.host.match(/^dynamodb\.(.*)\.amazonaws\.com/)) {
+        return;
+      }
+
+      print.warning();
+    },
+  })
+);
+
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+
+// Clean up after the tests are finished.
+afterAll(() => server.close());
