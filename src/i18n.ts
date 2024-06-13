ac3affa54b16870eb1011def2ed8bf261a3da735
Rui Ramos <ruiramos@gmail.com>
2024-06-13 12:27:24 +0100

    Set default language when no language set in group

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,13 +1,15 @@
 import i18next from 'i18next';
 import Backend from 'i18next-fs-backend';
 
+export const DEFAULT_LANGUAGE = 'en';
+
 i18next.on('failedLoading', function (lng, ns, msg) {
   console.log('[i18next] Failed to load:', { lng, ns });
   console.log(msg);
 });
 
 i18next.use(Backend).init({
-  lng: 'en',
+  lng: DEFAULT_LANGUAGE,
   debug: false,
   ns: 'translation',
   fallbackLng: ['en'],
--- src/middlewares/setLanguage.ts
+++ src/middlewares/setLanguage.ts
@@ -3,6 +3,7 @@ import { Context, NextFunction } from 'grammy';
 import { isGroup } from '../utils';
 import { getConfigForGroup } from '../config';
 import { MyContext } from '../bot';
+import { DEFAULT_LANGUAGE } from '../i18n';
 
 export default async function setLanguage(
   ctx: MyContext,
@@ -12,10 +13,14 @@ export default async function setLanguage(
     const chatId = ctx.chat.id;
     const config = await getConfigForGroup(chatId);
     if (config && config.language) {
-      console.log('Changing my language to', config.language);
+      console.log('Config setting language to', config.language);
       await i18next.changeLanguage(config.language);
+      ctx.config = { language: config.language };
+    } else {
+      // no language set, will set to default langauge (english at this point)
+      await i18next.changeLanguage(DEFAULT_LANGUAGE);
+      ctx.config = { language: DEFAULT_LANGUAGE };
     }
-    ctx.config = { language: config?.language ?? 'en' };
   }
 
   await next();
