28c1f5cb2b18cbc617476ac0436add382d607ebd
Rui Ramos <ruiramos@gmail.com>
2024-07-11 11:08:39 +0100

    actually use markdownv2 for config again

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -32,7 +32,7 @@ export const configCommand = async (ctx: CommandContext<MyContext>) => {
             language: groupConfig.language || DEFAULT_LANGUAGE,
           }),
           {
-            parse_mode: 'Markdown',
+            parse_mode: 'MarkdownV2',
           }
         );
       } else {
