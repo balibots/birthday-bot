640ebcb7a64433483b3e831b64a1366c651381ed
Rui Ramos <ruiramos@gmail.com>
2026-02-07 12:43:43 +0000

    include username on reply

--- src/commands/feedback.ts
+++ src/commands/feedback.ts
@@ -7,10 +7,11 @@ const FEEDBACK_GROUP_ID = process.env.IS_DEV ? '-5098456086' : '-1002209806645';
 
 export const feedbackCommand = async (ctx: MyContext) => {
   const text = ctx.match;
+  const username = ctx.message?.from.username;
 
   if (!text) {
     const msg = await ctx.reply(
-      'Please let us know your feedback by replying to this message:',
+      `@${username} please let us know your feedback by replying to this message.`,
       {
         reply_parameters: { message_id: ctx.message!.message_id },
         reply_markup: { force_reply: true, selective: true },
